40f2df427b453bb28c6139914b69cdcb2e6cddc4
snacksnackgo <2232865173@qq.com>
2020-06-11 16:44:45 +0800

    update

---
+++ demo/src/main/java/com/pingli/demo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.pingli.demo;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.text)
+    TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        text.setText("0x" + text.getId());
+    }
+}
